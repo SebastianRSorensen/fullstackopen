eed9b62a6d85919d0efd9202c4dac0a9ea9d7bbb
Sebastian Rosnes Sørensen <133763422+SebastianRSorensen@users.noreply.github.com>
2024-01-17 15:02:33 +0100

    1.12 done

--- part1d/src/App.jsx
+++ part1d/src/App.jsx
@@ -1,81 +1,31 @@
 import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
+    const anecdotes = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.'
+    ]
 
-  const handleGoodClick = () => {
-    setGood(good + 1)
-    setAll(all + 1)
-  }
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    setAll(all + 1)
-  }
-  const handleBadClick = () => {
-    setBad(bad + 1)
-    setAll(all + 1)
-  }
+    const [selected, setSelected] = useState(0)
 
-  return (
-    <div>
-      <h2>give feedback</h2>
-      <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleNeutralClick} text="neutral" />
-      <Button handleClick={handleBadClick} text="bad" />
-      <h2> statistics </h2>
-      <Display good={good} neutral={neutral} bad={bad} all={all} />
-    </div>
-  )
-}
+    const handleNext = () => {
+        const res = Math.floor(Math.random() * anecdotes.length)
+        console.log(res)
+        return res
+    }
 
-const Display = (props) => {
-  if (props.all === 0) {
     return (
-      <div>
-        <p>No feedback given</p>
-      </div>
+        <div>
+            <p>{anecdotes[selected]}</p>
+            <button onClick={() => setSelected(handleNext())}>next anecdote</button>
+        </div>
     )
-  }
-  return (
-    <div>
-      <Statistics {...props} />
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={handleClick}> {text} </button>
-  )
-}
-const Statistics = ({ good, neutral, bad, all }) => {
-  const posPercent = (good / all) * 100
-  const avg = (good - bad) / all
-  return (
-    <table>
-      <tbody>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={avg} />
-      <StatisticLine text="positive" value={posPercent + " %"} />
-      </tbody>
-    </table>
-  )
-}
-
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
 }
 
 export default App
---
+++ part1d/src/App6to11.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <h2> statistics </h2>
+      <Display good={good} neutral={neutral} bad={bad} all={all} />
+    </div>
+  )
+}
+
+const Display = (props) => {
+  if (props.all === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Statistics {...props} />
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}> {text} </button>
+  )
+}
+const Statistics = ({ good, neutral, bad, all }) => {
+  const posPercent = (good / all) * 100
+  const avg = (good - bad) / all
+  return (
+    <table>
+      <tbody>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={avg} />
+      <StatisticLine text="positive" value={posPercent + " %"} />
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+export default App
