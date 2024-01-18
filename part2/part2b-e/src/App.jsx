e2682c67765a6c06bdbe9d4005d411726c099cd9
Sebastian Rosnes Sørensen <133763422+SebastianRSorensen@users.noreply.github.com>
2024-01-18 15:56:33 +0100

    2.11 done

--- part2/part2b-e/src/App.jsx
+++ part2/part2b-e/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filterName, setFilter] = useState('')
@@ -57,9 +65,9 @@ const App = () => {
             <div>debug nr: {newNumber}</div>
             <Filter handleNewFilter={handleNewFilter} />
             <h3>Add a new</h3>
-            <PersonForm newName={newName} newNumber={newNumber} handleNewName={handleNewName} handleNewNumber={handleNewNumber} handleSubmit={handleSubmit}/>
+            <PersonForm newName={newName} newNumber={newNumber} handleNewName={handleNewName} handleNewNumber={handleNewNumber} handleSubmit={handleSubmit} />
             <h3>Numbers</h3>
-            <Persons personsToShow={personsToShow}/>
+            <Persons personsToShow={personsToShow} />
         </div>
     )
 }
