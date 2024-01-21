02d31b1078cf6b199bb0eec8ed528a7f65690d82
Sebastian Rosnes Sørensen <133763422+SebastianRSorensen@users.noreply.github.com>
2024-01-21 15:51:48 +0100

    2.18 done

--- part2/part2e/src/components/Country.jsx
+++ part2/part2e/src/components/Country.jsx
@@ -1,8 +1,16 @@
 const Country = (country) => {
-    console.log(country.country)
+    const currentCountry = country.country[0]
     return (
         <div>
-            <h1>Country</h1>
+            <h1>{currentCountry.name.common}</h1>
+            <p>capital {currentCountry.capital[0]}</p>
+            <p>area {currentCountry.area}</p>
+            <b>languages:</b>
+            <ul>
+                {Object.values(currentCountry.languages).map((language, index) =>
+                    <li key={language + index}>{language}</li>)}
+            </ul>
+            <img src={currentCountry.flags.png} alt="flag" width="200" height="200"></img>
         </div>
     )
 }
